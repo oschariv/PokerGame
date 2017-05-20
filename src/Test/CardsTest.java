2c5677ae4cd20e657875f34f8f1b914a690083ab
Oscar Charro Rivera <oschariv@gmail.com>
2017-05-20 14:17:12 +0200

    Añadios test basicos y clase Deck(Baraja)

--- src/Test/CardsTest.java
+++ src/Test/CardsTest.java
@@ -1,11 +1,15 @@
 package Test;
 
-
 import static org.junit.Assert.*;
 // Imports de las clase a comprobar!!!!
 import main.Card;
 
 import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.not;
 
 public class CardsTest {
 
@@ -36,4 +40,69 @@ public class CardsTest {
 		Card.Rank expRank = null;
 		Card instance = new Card(expSuit, expRank);
 	}
+
+	private static Card[] getAllCards() {
+		Card[] result = new Card[Card.Suit.values().length * Card.Rank.values().length];
+		int i = 0;
+		for (Card.Suit suit : Card.Suit.values()) {
+			for (Card.Rank rank : Card.Rank.values()) {
+				Card c = new Card(suit, rank);
+				result[i] = c;
+				i++;
+			}
+		}
+		return result;
+	}
+
+	@Test
+	public void testHashCode() {
+		System.out.println("hashCode");
+		Card[] allCards = getAllCards();
+		Set<Integer> hashCodes = new HashSet<>(allCards.length);
+		for (Card card : allCards) {
+			assertThat(hashCodes, not(hasItem(card.hashCode())));
+		}
+	}
+
+	@Test
+	public void testEqualsOtherObjects() {
+		System.out.println("equalsOtherObjects");
+		Card card = new Card(Card.Suit.CLUB, Card.Rank.ACE);
+		assertNotEquals("card: " + card + " != null", card, null);
+		assertNotEquals("card: " + card + " != 0", card, 0);
+		assertNotEquals("card: " + card + " != \"2C\"", card, "2C");
+	}
+
+	@Test
+	public void testEquals() {
+		System.out.println("equals");
+		int i = 0;
+		for (Card card0 : getAllCards()) {
+			int j = 0;
+			for (Card card1 : getAllCards()) {
+				if (i == j) {
+					assertEquals(card0, card1);
+				}
+				j++;
+			}
+			i++;
+		}
+	}
+
+	@Test
+	public void testEqualsDistinct() {
+		System.out.println("equals distinct");
+		int i = 0;
+		for (Card card0 : getAllCards()) {
+			int j = 0;
+			for (Card card1 : getAllCards()) {
+				if (i != j) {
+					assertNotEquals(card0, card1);
+				}
+				j++;
+			}
+			i++;
+		}
+	}
+
 }
